9339360a7d636ba670bfe2300e6de3fd0842bc21
Park, Jung-Uk <dnr1105@gmail.com>
2016-01-06 19:45:07 +0900

    version information added

--- src/com/mypcr/constant/Constants.java
+++ src/com/mypcr/constant/Constants.java
@@ -3,5 +3,5 @@ package com.mypcr.constant;
 public class Constants {
 	public static final String ADMIN_PASSWORD = "hsnl1210";
 	public static final String TESTMODE1_PASSWORD = "hsnl1210_test1";
-	public static final String SOURCE_VERSION = "V2.5";
+	public static final String SOURCE_VERSION = "V2.52";
 }
